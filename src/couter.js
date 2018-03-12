b4458b49dad2aefca1c953589b46bb538f475ffe
erisvaldo <erisvaldo.fsj@gmail.com>
2018-03-12 11:36:03 -0300

    count using Local storage and DidMount, DidUpdate

--- src/couter.js
+++ src/couter.js
@@ -5,8 +5,19 @@ class Counter extends React.Component{
         this.handleMinusOne = this.handleMinusOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.state = {
-            count: props.count,
-            name: props.name
+            count: 0
+        }
+    }
+    componentDidMount(){
+        const stringCount =  localStorage.getItem('count')
+        const count = parseInt(stringCount, 10)
+        if(!isNaN(count)){
+            this.setState(()=> ({ count}))
+        }
+    }
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.count !== this.state.count){
+            localStorage.setItem('count', this.state.count)
         }
     }
     handleAddOne(){
@@ -41,10 +52,6 @@ class Counter extends React.Component{
             </div>
         )
     }
-}
-Counter.defaultProps={
-    count: 0,
-    name: 'julie'
 }
 
 ReactDOM.render(<Counter/>, document.getElementById('app'))
