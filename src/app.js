112d6dc7e09723c5d47a4a40a9123a1c3b52b626
erisvaldo <erisvaldo.fsj@gmail.com>
2018-03-13 15:51:38 -0300

    begin styles, with scss

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+//normalize css for all browsers
+import 'normalize.css/normalize.css'
+//styles
+import './styles/styles.scss'
 //components
 import IndecisionApp from './components/IndecisionApp'
 // end components
